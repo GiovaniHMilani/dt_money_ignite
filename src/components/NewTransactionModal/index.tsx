074a0d3d3d5693db03523c5b0576e1e7a991dcde
Milani <milani@Mac-Pro-de-Milani.local>
2021-03-24 08:58:08 -0300

    feat: styles in transactionModal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,44 @@
+import Modal from "react-modal";
+import { Container } from "./styles";
+
+import closeImg from "../../assets/close.svg";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+Modal.setAppElement("#root");
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transacao</h2>
+
+        <input placeholder="Titulo" />
+
+        <input placeholder="Valor" type="number" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
